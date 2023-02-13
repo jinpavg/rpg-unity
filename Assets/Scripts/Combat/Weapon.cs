db2d9ad32653b4a48370e6f74eb6604680a46829
Alex Van Gils <alex@cycling74.com>
2023-02-12 20:35:30 -0500

    arrows

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -1,3 +1,4 @@
+using RPG.Core;
 using UnityEngine;
 
 namespace RPG.Combat
@@ -10,18 +11,37 @@ namespace RPG.Combat
         [SerializeField] AnimatorOverrideController animatorOverride = null;
         [SerializeField] float weaponRange = 2f;
         [SerializeField] float weaponDamage = 5f;
+        [SerializeField] bool isRightHanded = true;
+        [SerializeField] Projectile projectile = null;
 
-        public void Spawn(Transform handTransform, Animator animator)
+        public void Spawn(Transform rightHand, Transform leftHand, Animator animator)
             {
                 if (equippedPrefab != null)
-                {
-                    Instantiate(equippedPrefab, handTransform);
-                }
-                if (animatorOverride != null)
+            {
+                Transform handTransform = GetTransform(rightHand, leftHand);
+                Instantiate(equippedPrefab, handTransform);
+            }
+            if (animatorOverride != null)
                 {
                     animator.runtimeAnimatorController = animatorOverride;
                 }
             }
+
+        private Transform GetTransform(Transform rightHand, Transform leftHand)
+        {
+            return isRightHanded ? rightHand : leftHand;
+        }
+
+        public bool HasProjectile()
+        {
+            return projectile != null;
+        }
+
+        public void LaunchProjectile(Transform rightHand, Transform leftHand, Health target)
+        {
+            Projectile projectileInstance = Instantiate(projectile, GetTransform(rightHand, leftHand).position, Quaternion.identity);
+            projectileInstance.SetTarget(target, weaponDamage);
+        }
 
         public float GetRange()
         {
